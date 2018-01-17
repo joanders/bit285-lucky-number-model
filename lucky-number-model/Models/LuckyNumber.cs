21fcfa91e5d8fe7d5d99d74edde694ab686af932
Brian Bansenauer <brianb@cascadia.edu>
2018-01-17 12:46:56 -0800

    Addding View and Model

---
+++ lucky-number-model/Models/LuckyNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace lucky_number_model.Models
+{
+    public class LuckyNumber
+    {
+        [Required]
+        [Display(Name = "Lucky Number")]
+        [Range(1, 9, ErrorMessage = "Number must be between 1 and 9")]
+        public int Number { get; set; } // the user's choice of a lucky number
+
+        [Required]
+        public decimal Balance { get; set; } // the balance for this game
+
+        public Boolean isWinner { get; set; } // a flag for when the lucky number matches the spin
+    }
+}
